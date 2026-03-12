31d195a4ed39fddb9a33e5bbb7c118339d983fe2
Berat <bkkilic2016@gmail.com>
2026-03-12 16:48:52 +0300

    devir

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = `https://dkdehxcrpdfshtqmpiaq.supabase.co`;
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRrZGVoeGNycGRmc2h0cW1waWFxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzEyNjQ1NzgsImV4cCI6MjA4Njg0MDU3OH0.D-wUiK_gBKHRqJXZN0KoGIz39LadvXQsd1ZBniSRrF8";
-//const SUPABASE_ANON_KEY = "sb_publishable_zDSMAg-F6f51qWWYWbE93g_TtSI5olA";
+const SUPABASE_URL = `https://qrbakqngrkecuvrcjmxx.supabase.co`;
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFyYmFrcW5ncmtlY3V2cmNqbXh4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMzMTYyMzEsImV4cCI6MjA4ODg5MjIzMX0.sNNqTi6iG4Jjr9TpHBemNeMfFzKNU2c1xm9evPow_yU"
+//const SUPABASE_ANON_KEY = "sb_publishable_yw0o1sIDR-O-Y1cQN6wDTA_kNG01gCz";
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
   auth: {
